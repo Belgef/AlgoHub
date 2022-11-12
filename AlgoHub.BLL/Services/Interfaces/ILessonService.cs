19a49eb85377bd0a1ac2e8d1fb921a4140cdc411
Belgef <girnyimykola@gmail.com>
2022-11-13 00:35:22 +0200

    Added basic service

---
+++ AlgoHub.BLL/Services/Interfaces/ILessonService.cs
@@ -0,0 +1,14 @@
+﻿using AlgoHub.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoHub.BLL.Services.Interfaces
+{
+    public interface ILessonService
+    {
+        public IEnumerable<Lesson> GetTopPopularLessons(int n);
+    }
+}
---
+++ AlgoHub.BLL/Services/LessonService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoHub.BLL.Services.Interfaces;
+using AlgoHub.DAL.Entities;
+using AlgoHub.DAL.Repository;
+
+namespace AlgoHub.BLL.Services
+{
+    public class LessonService : ILessonService
+    {
+        private readonly IRepository<Lesson> _repository;
+
+        public LessonService(IRepository<Lesson> repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Lesson> GetTopPopularLessons(int n)
+        {
+            return _repository.AllIncluding(lesson => lesson.Author).OrderByDescending(l => l.Views).Take(n);
+        }
+    }
+}
