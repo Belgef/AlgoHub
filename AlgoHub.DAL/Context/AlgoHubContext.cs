3eaf5ff3d46bcbce3df2d0c8785e7ddf2cddb323
Belgef <girnyimykola@gmail.com>
2022-11-12 17:22:03 +0200

    Finished DAL scaffolding

--- AlgoHub.DAL/Context/AlgoHubContext.cs
+++ AlgoHub.DAL/Context/AlgoHubContext.cs
@@ -1,18 +1,19 @@
 ﻿using AlgoHub.DAL.Entities;
+using GachiHelp.DAL.Context;
 using Microsoft.EntityFrameworkCore;
 
 namespace AlgoHub.DAL.Context
 {
     public class AlgoHubContext : DbContext
     {
-        public DbSet<Lesson> Lessons { get; set; }
-        public DbSet<Problem> Problems { get; set; }
-        public DbSet<Solve> Solves { get; set; }
-        public DbSet<Test> Tests { get; set; }
-        public DbSet<ProblemComment> ProblemComments { get; set; }
-        public DbSet<SolveComment> SolveComments { get; set; }
-        public DbSet<Tag> Tags { get; set; }
-        public DbSet<Language> Languages { get; set; }
+        public DbSet<Lesson> Lessons { get; set; } = null!;
+        public DbSet<Problem> Problems { get; set; } = null!;
+        public DbSet<Solve> Solves { get; set; } = null!;
+        public DbSet<Test> Tests { get; set; } = null!;
+        public DbSet<ProblemComment> ProblemComments { get; set; } = null!;
+        public DbSet<SolveComment> SolveComments { get; set; } = null!;
+        public DbSet<Tag> Tags { get; set; } = null!;
+        public DbSet<Language> Languages { get; set; } = null!;
 
         public AlgoHubContext(DbContextOptions<AlgoHubContext> options) : base(options) { }
 
---
+++ AlgoHub.DAL/Context/SeedData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using AlgoHub.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GachiHelp.DAL.Context
+{
+    internal static class SeedData
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+
+        }
+        private static string HashPassword(string password) => Convert.ToBase64String(SHA256.HashData(Encoding.Default.GetBytes(password)));
+    }
+}
